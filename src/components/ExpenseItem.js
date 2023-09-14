b16871e1fa4c75fd0b7aadee40a0e2b572c4b0a8
codingpartnerNB <nehab5314@gmail.com>
2023-09-14 22:49:01 +0530

    expense items

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+import './ExpenseItem.css';
+function ExpenseItem(){
+    return(
+        <div>
+            <h1 className="color-blue">Expense Items</h1>
+            <div>Food Rs 10</div>
+            <div>Petrol Rs 100</div>
+            <div>Movies Rs 200</div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
