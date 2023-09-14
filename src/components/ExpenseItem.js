70bbb3d3ff918789a9c02196a446d17c00671ae9
codingpartnerNB <nehab5314@gmail.com>
2023-09-14 23:24:01 +0530

    Using variable

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
 import './ExpenseItem.css';
 function ExpenseItem(){
+    const LocationOfExpenditure = "Delhi";
     return(
         <div>
             <h1 className="color-blue">Expense Items</h1>
             <div>Food Rs 10</div>
             <div>Petrol Rs 100</div>
             <div>Movies Rs 200</div>
+            <div>Location of expenditure is {LocationOfExpenditure}</div>
         </div>
     );
 }
