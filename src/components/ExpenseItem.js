b04ca0aa376a39aed2f0fa3bc5b553edb962be7e
codingpartnerNB <nehab5314@gmail.com>
2023-09-18 19:49:16 +0530

    Adding update title and expense button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,27 @@
+import React, {useState} from "react";
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseItemDetails/ExpenseDate';
 import ExpenseDetails from './ExpenseItemDetails/ExpenseDetails';
 const ExpenseItem = (props) => {
-    const deleteHandler = () =>{
-        console.log("Button Clicked!!");
+    const [title, setTitle] = useState(props.title);
+    const [expense, setExpense] = useState(props.amount);
+    // const deleteHandler = () =>{}
+    const titleHandler = () =>{
+        setTitle("Updated!!")
+        console.log(title);
+    }
+    const expenseHandler = () =>{
+        setExpense(100);
+        console.log(expense);
     }
     return(
         <div className='container'>
             <h1 className="color-blue">Expense Items</h1>
             <ExpenseDate date={props.date} />
-            <ExpenseDetails title={props.title} amount={props.amount} locationForExpense={props.locationForExpense} />
-            <button id="delBtn" onClick={deleteHandler}>Delete Expense</button>
+            <ExpenseDetails title={title} amount={expense} locationForExpense={props.locationForExpense} />
+            {/* <button className="btn" onClick={deleteHandler}>Delete Expense</button> */}
+            <button className="btn" onClick={titleHandler}>Update Title</button>
+            <button className="btn" onClick={expenseHandler}>Update Expense</button>
         </div>
     );
 }
