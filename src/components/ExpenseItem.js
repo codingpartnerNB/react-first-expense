9201acdbf415214d785f0f357c7490743f013000
codingpartnerNB <nehab5314@gmail.com>
2023-09-18 17:20:30 +0530

    Adding delete button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,11 +2,15 @@ import './ExpenseItem.css';
 import ExpenseDate from './ExpenseItemDetails/ExpenseDate';
 import ExpenseDetails from './ExpenseItemDetails/ExpenseDetails';
 const ExpenseItem = (props) => {
+    const deleteHandler = () =>{
+        console.log("Button Clicked!!");
+    }
     return(
         <div className='container'>
             <h1 className="color-blue">Expense Items</h1>
             <ExpenseDate date={props.date} />
             <ExpenseDetails title={props.title} amount={props.amount} locationForExpense={props.locationForExpense} />
+            <button id="delBtn" onClick={deleteHandler}>Delete Expense</button>
         </div>
     );
 }
