563cddd8e24664f034597604a4e1bc013f08a879
codingpartnerNB <nehab5314@gmail.com>
2023-09-20 01:27:19 +0530

    Storing form data using state hook

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import './ExpenseForm.css';
 
 const ExpenseForm = () =>{
-    const titleExpenseHandler = (event)=>{
-        console.log(event.target.value);
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+    const titleChangeHandler = (event)=>{
+        setEnteredTitle(event.target.value);
+    }
+    const amountChangeHandler = (event)=>{
+        setEnteredAmount(event.target.value);
+    }
+    const dateChangeHandler = (event)=>{
+        setEnteredDate(event.target.value);
     }
     return(
         <div className="form">
             <form className="new-expense">
                 <div className="expense-control">
                     <label>Expense Title</label>
-                    <input type="text" onChange={titleExpenseHandler} />
+                    <input type="text" onChange={titleChangeHandler} />
                 </div>
                 <div className="expense-control">
                     <label>Expense Amount</label>
-                    <input type="number" className="amount-expense" />
+                    <input type="number" onChange={amountChangeHandler} />
                 </div>
                 <div className="expense-control">
                     <label>Date</label>
-                    <input type="date" />
+                    <input type="date" onChange={dateChangeHandler} />
                 </div>
                 <div className="exp-btn">
                     <button type="submit" className="expense-btn">Add Expense</button>
