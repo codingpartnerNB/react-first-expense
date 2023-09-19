6ed41985dd0d3d3ba8f48ebdecc1883bf6c5f96b
codingpartnerNB <nehab5314@gmail.com>
2023-09-20 01:56:09 +0530

    Working with multiple states

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -5,14 +5,50 @@ const ExpenseForm = () =>{
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
+
+    //We can also use this to store values
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle:'',
+    //     enteredAmount:'',
+    //     enteredDate:''
+    // });
+
     const titleChangeHandler = (event)=>{
         setEnteredTitle(event.target.value);
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle:event.target.value
+        // });
+        //This approach is better and safe than above
+        // setUserInput((prevState)=>{
+        //     return {...prevState,enteredTitle:event.target.value};
+        // });
     }
     const amountChangeHandler = (event)=>{
         setEnteredAmount(event.target.value);
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount:event.target.value
+        // });
+        //This approach is better and safe than above
+        // setUserInput((prevState)=>{
+        //     return {...prevState,enteredAmount:event.target.value};
+        // });
     }
     const dateChangeHandler = (event)=>{
         setEnteredDate(event.target.value);
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate:event.target.value
+        // });
+
+        //This approach is better and safe than above
+        // setUserInput((prevState)=>{
+        //     return {...prevState,enteredDate:event.target.value};
+        // });
     }
     return(
         <div className="form">
