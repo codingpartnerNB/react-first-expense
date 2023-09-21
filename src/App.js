6e54692e94462dccb490e6be7d8fe6a8fae74a00
codingpartnerNB <nehab5314@gmail.com>
2023-09-22 00:02:22 +0530

    Adding filter that doesn't work

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 import NewExpense from './components/ExpenseForm/NewExpense';
+import ExpenseFilter from './components/ExpenseItemDetails/ExpenseFilter';
 
 const App = () => {
   // const expenses = [{
@@ -18,15 +19,21 @@ const App = () => {
   //   date:new Date()
   // }];
 
+
+  const [filteredYear, setFilteredYear] = useState('2020');
   const [expenses, setExpenses] = useState([]);
   const LocationOfExpenditure = "Delhi";
 
   const addExpenseHandler = (expense)=>{
     setExpenses([{...expense}]);
   }
+  const filterChangeHandler = (selectedYear)=>{
+    setFilteredYear(selectedYear);
+  }
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       {
         expenses.map((item)=>{
         return <ExpenseItem title={item.title} amount={item.amount} date={item.date} locationForExpense={LocationOfExpenditure}></ExpenseItem>
---
+++ src/components/ExpenseItemDetails/ExpenseFilter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ExpenseFilter = (props)=>{
+    const dropDownChangeHandler = (event)=>{
+        props.onChangeFilter(event.target.value);
+    }
+    return(
+        <div>
+            <label>Filter By Year</label>
+            <select value={props.selected} onChange={dropDownChangeHandler}>
+            <option value="2024">2024</option>
+            <option value="2023">2023</option>
+            <option value="2022">2022</option>
+            <option value="2021">2021</option>
+            <option value="2020">2020</option>
+            <option value="2019">2019</option>
+            </select>
+        </div>
+    );
+}
+
+export default ExpenseFilter;
